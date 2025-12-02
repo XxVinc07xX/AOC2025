72fe8e808de8550ff252914f61ab31cc5e7a1cb0
vw <vincent.wuillaume@student.uclouvain.be>
2025-12-02 09:02:13 +0100

    pt 2

--- 2/solve.py
+++ 2/solve.py
@@ -19,8 +19,42 @@ def check_half(nb):
     return False
 
 #print(check_half(5251))
+    
+def split_equal(str_nb, n):
+    l = []
+    jump = len(str_nb)//n
+    #print(jump)
+    start = 0
+    for _ in range(0,n):
+        l.append(str_nb[start:start+jump])
+        start += jump
+    return l 
+
+def check_l(l):
+    res = len(set(l)) == 1
+    return res
+
+def check_all(nb):
+    l = []
+    string_nb = str(nb)
+    for i in range(2, len(string_nb)+1):
+        if len(string_nb)%i != 0:
+            continue
+        l = split_equal(string_nb, i)
+        if check_l(l):
+            print(l)
+            return True
+        #return False
+    
+#print(check_all(12))
+
+#print(split_equal('11111111', 7))
+print(check_l(split_equal('565656', 3)))
+
 
 #===================END FUNCTIONS=======================
+
+#PART1
 
 l = lines.split(",")
 ranges = []
@@ -39,7 +73,21 @@ for r in ranges:
         if val:
             sum += i
 
-print(sum)
+#print(sum)
+
+
+#PART2
+sum2 = 0
+for r in ranges:
+    for i in range(r[0], r[1]+1):
+        val = check_all(i)
+        if val:
+            #print(i)
+            sum2 += i
+
+print(sum2)
+
+
 
 
 
