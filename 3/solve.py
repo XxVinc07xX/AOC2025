2fa1af714713bcaa9c293670c2f96dcdce1dc372
vw <vincent.wuillaume@student.uclouvain.be>
2025-12-03 11:45:41 +0100

    pt 2

--- 3/solve.py
+++ 3/solve.py
@@ -33,10 +33,13 @@ for line in lines:
     #print(best)
     sum += best
 
-#print(sum)
+print(sum)
 
 ###PART2
 
+'''
+found each time the maximum in the sublist starting after current best index and endind with enough element left in case best is last element of the sublist
+Example: 111111111119111 => 111111119111; 9 cannot be the first number since there is not enough digit left to form a 12 digit number'''
 sum = 0
 for line in lines:
     line = line.removesuffix("\n")
@@ -44,38 +47,29 @@ for line in lines:
     cur_best_idx = None
     cur = ''
     
-    #donner l[:-11] : trouver max
-    #donner l[max:-10]: trouver max
-    #donner l[max:-9]: trouver max 
-    #...
     for _ in range(len(line)):
         if cur_best_idx == None:
             sub = line[:-11]
             start_sub_idx = line.index(line[0])
             end_sub_idx = line.index(line[-1])
         else:
-            #print(cur_best_idx, found)
             if (-11+found) != 0: 
                 sub = line[cur_best_idx+1:-11+found]
             else:
                 sub = line[cur_best_idx+1:]
-            #print("sub = " +  str(sub))
             start_sub_idx = cur_best_idx+1
             end_sub_idx = len(line)-11+found-1
-            #print(start_sub_idx, end_sub_idx)
-            #print("sub = " + str(sub))
-        #print("sub = " +  str(sub))
+
         maxi = max_idx(sub)
         cur += sub[maxi]
         found += 1
-        #print(start_sub_idx, maxi)
         cur_best_idx = start_sub_idx + maxi
         if found == 12:
             best = int(cur)
             break
 
     best = int(cur)
-    print("best = " + str(best))
+    #print("best = " + str(best))
     sum += best
 
 print(sum)
