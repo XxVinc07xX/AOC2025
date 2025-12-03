79bf1a5033ec25504b44a87aaa9f02514d0ef408
vw <vincent.wuillaume@student.uclouvain.be>
2025-12-03 15:56:24 +0100

    pt 2

--- 1/solve.py
+++ 1/solve.py
@@ -10,6 +10,24 @@ for line in lines:
     #print(direction, rotation)
 
     if direction == 'L':
+        cur = (cur-rotation) % 100
+        
+    elif direction == 'R':
+        cur = (cur+rotation) % 100
+    #print(cur)
+    if cur == 0:
+        count += 1
+print(count)
+
+cur = 50
+count = 0
+for line in lines:
+    line = line.removesuffix("\n")
+    direction = line[0]
+    rotation = int(line[1:])
+    #print(direction, rotation)
+
+    if direction == 'L':
         for i in range(rotation):
             cur = (cur-1) % 100
             if cur == 0:
