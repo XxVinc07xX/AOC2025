650cb3b5611051d5be6a1353e72433833bc8d4af
vw <vincent.wuillaume@student.uclouvain.be>
2025-12-10 15:58:49 +0100

    inneficient pt 2

--- 10/solve.py
+++ 10/solve.py
@@ -53,5 +53,63 @@ for line in lines:
 
 print(count)
 
+###PART2 -> working on test example, not on puzzle input
 
-        
+import copy
+
+def press_button(cur_voltage, to_push, target):
+
+    ret = copy.deepcopy(cur_voltage)
+
+    for btn in to_push:
+        ret[btn] += 1
+
+        if ret[btn] > target[btn]:
+            #print("here")
+            return None
+
+    return ret
+
+count = 0
+for line in lines:
+    line = line.removesuffix("\n").split(" ")
+    wiring = []
+    for item in line:
+
+        if item.startswith('('):
+            inter = []
+            for i in range(1,len(item)-1):
+                if item[i].isdigit():
+                    inter.append(int(item[i]))
+            wiring.append(inter)
+
+        elif item.startswith('{'):
+            item = item[1:-1]
+            item = item.split(",")
+            voltage_target = []
+            for i in range(len(item)):
+                if item[i].isdigit():
+                    voltage_target.append(int(item[i]))
+
+    len_light = len(voltage_target)
+    start = [0] * len_light
+
+    visited = []
+    to_visit = [[start, 0]]
+
+    while len(to_visit) != 0:
+
+        inter_voltage, nb_btn_pressed = to_visit.pop(0)
+
+        if inter_voltage == voltage_target:
+            count += nb_btn_pressed
+            break
+
+        for i in range(len(wiring)):
+            res = press_button(inter_voltage, wiring[i], target=voltage_target)
+            if res:
+                if res not in visited:
+                    visited.append(res)
+                    to_visit.append([res, nb_btn_pressed+1])
+
+print(count)
